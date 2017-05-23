57477095f372ebf9f45a2a4327eeaddbbc90f9ad
Alex <alex.porciani@mail.polimi.it>
2017-05-23 17:31:39 +0200

    json advanced

--- src/main/java/it/polimi/ingsw/GC_21/GAMECOMPONENTS/CardCreator.java
+++ src/main/java/it/polimi/ingsw/GC_21/GAMECOMPONENTS/CardCreator.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw.GC_21.GAMECOMPONENTS;
 
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -13,52 +13,84 @@ import org.json.simple.parser.ParseException;
 import it.polimi.ingsw.GC_21.EFFECT.Immediate;
 
 public class CardCreator {
-
-public void cardCreate() {
 	
-     try {
-    	 JSONParser parser = new JSONParser();
-         Object obj = parser.parse(new FileReader("C:\\Users\\Alex\\workspace\\prova-finale-template\\provajs.json"));
-         JSONObject card = (JSONObject) obj;
-         JSONArray cardarray= (JSONArray) card.get("Card");
-       
-         for (Object o : cardarray) {
-             JSONObject jsonLineItem = (JSONObject) o;
-             
-             if("Dev".equals((String) jsonLineItem.get("type"))) {
-            	
-            	 DevelopmentCard devCardCreating = new DevelopmentCard((String) jsonLineItem.get("name"));
-            	 devCardCreating.setAge(Integer.parseInt(jsonLineItem.get("age").toString()));
-          	     devCardCreating.setDevCardType(DevCardType.valueOf(jsonLineItem.get("DevType").toString()));
-            	 this.create(devCardCreating, jsonLineItem);             
-             }
-             else if("Ex".equals((String) jsonLineItem.get("type"))) {
-            	 ExcommunicationCard excommunicationCard = new ExcommunicationCard((String) jsonLineItem.get("name"));
-            	 excommunicationCard.setAge(Integer.parseInt(jsonLineItem.get("age").toString()));
-            	 this.create(excommunicationCard, jsonLineItem); 
-             }
-             else if("Lead".equals((String) jsonLineItem.get("type"))) {
-            	 LeaderCard leaderCard = new LeaderCard((String) jsonLineItem.get("name"));
-            	 leaderCard.setDescription((String) jsonLineItem.get("desc"));
-            	 this.create(leaderCard, jsonLineItem);
-             }
-          }
-             
-         
-     }         
-
-         catch (FileNotFoundException e) {
+	private ArrayList<Card> cards;
+	private JSONParser parser = new JSONParser();
+   
+	
+	public ArrayList<Card> devCardsCreate(DevCardType devCardType, int age) {
+		try {
+		Object obj = parser.parse(new FileReader("C:\\Users\\Alex\\workspace\\prova-finale-template\\provajs.json"));
+	    JSONObject card = (JSONObject) obj;
+	    JSONArray cardarray= (JSONArray) card.get("DevCard");
+	    for (Object o : cardarray) {
+           	JSONObject jsonLineItem = (JSONObject) o;       
+	    	if(Integer.parseInt(jsonLineItem.get("age").toString())==age && devCardType.toString().equals(DevCardType.valueOf(jsonLineItem.get("DevType").toString()))){              	
+           	 DevelopmentCard devCardCreating = new DevelopmentCard((String) jsonLineItem.get("name"));
+           	 devCardCreating.setAge(age);
+         	 devCardCreating.setDevCardType(devCardType);
+           	 this.create(devCardCreating, jsonLineItem);
+           	 cards.add(devCardCreating);            	 
+            }
+	     }
+		} catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
         }
-     
-    
-	}	
+		return cards;
+	}
 	
-				public void create(Card cardcreating, JSONObject jsonLineItem) {
+	public ArrayList<Card> ExCardsCreate(int age) {
+		try {
+		Object obj = parser.parse(new FileReader("C:\\Users\\Alex\\workspace\\prova-finale-template\\provajs.json"));
+	    JSONObject card = (JSONObject) obj;
+	    JSONArray cardarray= (JSONArray) card.get("ExCard");
+	    for (Object o : cardarray) {
+           	JSONObject jsonLineItem = (JSONObject) o;       
+	    	if(Integer.parseInt(jsonLineItem.get("age").toString())==age){              	
+           	 ExcommunicationCard excommunicationCard = new ExcommunicationCard((String) jsonLineItem.get("name"));
+           	 excommunicationCard.setAge(age);
+         	 this.create(excommunicationCard, jsonLineItem);
+           	 cards.add(excommunicationCard);            	 
+            }
+	     }
+		} catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		return cards;
+	}
+	
+	public ArrayList<Card> LeadCardsCreate() {
+		try {
+		Object obj = parser.parse(new FileReader("C:\\Users\\Alex\\workspace\\prova-finale-template\\provajs.json"));
+	    JSONObject card = (JSONObject) obj;
+	    JSONArray cardarray= (JSONArray) card.get("LeadCard");
+	    for (Object o : cardarray) {
+           	JSONObject jsonLineItem = (JSONObject) o;     
+    	     LeaderCard leaderCard = new LeaderCard((String) jsonLineItem.get("name"));
+           	 this.create(leaderCard, jsonLineItem);
+           	 cards.add(leaderCard);            	 
+            }
+	     
+		} catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		return cards;
+	}
+	
+
+	public void create(Card cardcreating, JSONObject jsonLineItem) {
 
 	        	                
 	                JSONArray reqarray= (JSONArray) jsonLineItem.get("Req");
@@ -79,7 +111,7 @@ public void cardCreate() {
 	               Immediate immediate = new Immediate(Imm);
 	               cardcreating.setImmediateEffect(immediate);
 	             
-	            }
+	}
 
 	
 }
--- src/main/java/it/polimi/ingsw/GC_21/GAMECOMPONENTS/LeaderDeck.java
+++ src/main/java/it/polimi/ingsw/GC_21/GAMECOMPONENTS/LeaderDeck.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class LeaderDeck extends Deck {
 
-	public LeaderDeck(ArrayList<Card> cards) {
-		super(cards);
-		// TODO Auto-generated constructor stub
+	public LeaderDeck() {
+		cards = cardCreator.LeadCardsCreate();
 	}
+	
 }
