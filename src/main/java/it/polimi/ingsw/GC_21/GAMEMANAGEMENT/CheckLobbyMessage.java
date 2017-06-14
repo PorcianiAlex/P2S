f9a4a0dd2d4ee3bca60a665061db5ec2938afea5
Santa <davide2.santambrogio@mail.polimi.it>
2017-06-14 16:38:50 +0200

    Check Lobby Message

--- src/main/java/it/polimi/ingsw/GC_21/GAMEMANAGEMENT/CheckLobbyMessage.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.GC_21.GAMEMANAGEMENT;
-
-public class CheckLobbyMessage extends Message {
-
-	
-	
-	
-}
--- src/main/java/it/polimi/ingsw/GC_21/view/LobbyMessage.java
+++ src/main/java/it/polimi/ingsw/GC_21/view/LobbyMessage.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.GC_21.view;
 
 import java.util.ArrayList;
 
+import it.polimi.ingsw.GC_21.GAMEMANAGEMENT.CheckLoginMessage;
 import it.polimi.ingsw.GC_21.GAMEMANAGEMENT.Game;
 import it.polimi.ingsw.GC_21.GAMEMANAGEMENT.Message;
 import it.polimi.ingsw.GC_21.controller.ControllerManager;
@@ -27,15 +28,27 @@ public class LobbyMessage extends Message {
 		modelGame = controller.getModelGame();
 		if(create) {
 	        modelGame = controllerManager.createGame(controller);
+	        CheckLoginMessage checkLoginMessage = new CheckLoginMessage(true, "Lobby ok", null); //TODO create a superclass message
+	        remoteView.getAdapter().sendObject(checkLoginMessage);
 	        remoteView.createPlayer(modelGame); 
 	        letStart(); 
 	    } 
-	    else {  
-	      modelGame = controllerManager.getControllers().get(joined-1).getModelGame(); //take the selected game
-	      remoteView.createPlayer(modelGame); 
-	      modelGame.notifyString(remoteView.getPlayer().getName()+" joins the match! \nActual number of player: " + modelGame.getPlayers().size());
-	      remoteView.getAdapter().out("Waiting for the 'start' by the game host"); 
-	    }    
+	    else if (joined < controllerManager.getGames().size() && joined > 0) {
+	    	 modelGame = controllerManager.getGames().get(joined-1); //take the selected game
+		        CheckLoginMessage checkLoginMessage = new CheckLoginMessage(true, "Lobby ok", null); //TODO create a superclass message
+		        remoteView.getAdapter().sendObject(checkLoginMessage);
+		      remoteView.createPlayer(modelGame); 
+		      modelGame.notifyString(remoteView.getPlayer().getName()+" joins the match! \nActual number of player: " + modelGame.getPlayers().size());
+		      remoteView.getAdapter().out("Waiting for the 'start' by the game host");
+		}
+	    else {
+	        CheckLoginMessage checkLoginMessage = new CheckLoginMessage(false, "Your game choice is not valid", null); //TODO create a superclass message
+	        remoteView.getAdapter().sendObject(checkLoginMessage);
+
+
+		}   
+	      
+	       
 		return true;
 	}
 	
