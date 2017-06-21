44544027df333d01899469e08ebab34754861442
Alex <alex.porciani@mail.polimi.it>
2017-06-21 17:08:55 +0200

    mmmm

--- src/main/java/it/polimi/ingsw/GC_21/CLIENT/TurnMessage.java
+++ src/main/java/it/polimi/ingsw/GC_21/CLIENT/TurnMessage.java
@@ -58,7 +58,7 @@ public class TurnMessage extends MessageToClient{
 	
 	@Override
 	public void executeGUI(FXMLGameController gameController) {
-		gameController.refreshBoard(board, players);
+		gameController.refreshBoard(board, players, description);
 		
 	}
 	
