2c1da3c9b2d978d641973bfd69dfbda1eb028c38
Alex <alex.porciani@mail.polimi.it>
2017-07-09 22:29:52 +0200

    fix on mp

--- src/main/java/it/polimi/ingsw/GC_21/ACTION/TowerPlacement.java
+++ src/main/java/it/polimi/ingsw/GC_21/ACTION/TowerPlacement.java
@@ -116,7 +116,7 @@ public class TowerPlacement extends PlacementAction {
 		    }
 			OwnedCards ownedTerritoryCards = myPersonalBoard.getSpecificOwnedCards(DevCardType.Territory);
 			if(selectedCardType.equals(DevCardType.Territory) && !playerInAction.isCheckOnMP()
-					&& ownedTerritoryCards.getOwnedCardsnumber() > 2
+					&& ownedTerritoryCards.getOwnedCardsnumber() >= 2
 					&& !myPersonalBoard.getMyPossession().compare(ownedTerritoryCards.getMyDevCards()[ownedTerritoryCards.getOwnedCardsnumber()].getPossession())){
 				// check on MilitaryPoint Required taking a territoryCard. The requirement is saved in the correct personalCardPlace with the attribute Possession 
 				return false;
